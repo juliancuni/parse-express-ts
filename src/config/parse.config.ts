528e9ee368da3380f5f5f24b9a2fb1ccdce04117
Julian Cuni <julian.cuni@microservices.al>
2022-01-04 20:54:03 +0100

    Reset Password back to frontend

--- src/config/parse.config.ts
+++ src/config/parse.config.ts
@@ -35,14 +35,14 @@ const config = {
     javascriptKey: process.env.JS_KEY || "",
   
     customPages: {
-      // passwordResetSuccess: process.env.PASS_RESET_SUCCESS_LINK || "http://localhost:3000/auth/resetpassword",
+      passwordResetSuccess: process.env.PASS_RESET_SUCCESS_LINK || "http://localhost:3000/auth/resetpassword",
       verifyEmailSuccess: process.env.EMAIL_VERIFIED_LINK || "http://localhost:3000/auth/emailverified",
       // parseFrameURL: process.env.PARSE_FRAME_LINK "http://localhost:3000/auth/parseFrameURL",
       linkSendSuccess: process.env.SEND_LINK_SUCCESS || "http://localhost:3000/auth/linkSendSuccess",
       linkSendFail: process.env.SEND_LINK_FAIL || "http://localhost:3000/auth/linkSendFail",
-      invalidLink: process.env.INVALID_LINK || "http://localhost:3000/auth/invalidLink",
+      invalidLink: process.env.INVALID_LINK || "http://localhost:3000/auth/invalidlink",
       invalidVerificationLink: process.env.INVALID_VERIFICATION_LINK || "http://localhost:3000/auth/invalidVerificationLink",
-      // choosePassword: process.env.PASS_RESET_LINK || "http://localhost:3000/auth/choosePassword"
+      choosePassword: process.env.PASS_RESET_LINK || "http://localhost:3000/auth/passwordrecovery"
     },
     allowHeaders: ['X-Parse-Installation-Id', 'X-Parse-Client-Key']
   };
