5407b785d0410dfafd997df5628a84a5fac6511a
Julian Cuni <julian.cuni@microservices.al>
2022-01-11 18:04:15 +0100

    cors() helmet()

--- src/index.ts
+++ src/index.ts
@@ -3,27 +3,21 @@ import helmet from 'helmet';
 import cors from 'cors';
 import { router } from './routes';
 import config from './config/parse.config';
-// import path from 'path';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { default: ParseServer, ParseGraphQLServer } = require('parse-server');
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.setHeader("Cross-Origin-Resource-Policy", process.env.FRONT_END || "https://frontend-dev.apps.microservices.al");
-    res.setHeader('x-powered-by', 'Microservices Albania');
-    next();
-});
 app.use(cors());
 app.use(helmet());
 
+app.use((req, res, next) => {
+    res.setHeader("Cross-Origin-Resource-Policy", process.env.FRONT_END || "https://frontend-dev.apps.microservices.al");
+    // res.setHeader('x-powered-by', 'Microservices Albania');
+    next();
+});
 
 app.use("/api", router);
-// app.use(express.static(path.resolve("./public")));
-
-// app.get("/choosepassword", (req: Request, res: Response) => {
-//     res.sendFile(path.resolve("./templates/choosePassword.html"));
-// });
 
 app.get("/", (req: Request, res: Response) => {
     res.send("MAIN BACKEND SERVER --- MSA");
@@ -36,14 +30,12 @@ const parseGraphQLServer = new ParseGraphQLServer(
     parseServer,
     {
         graphQLPath: process.env.GRAPHQL_MOUNT || '/graphql',
-        // playgroundPath: '/playground'
     }
 );
 
 app.use(mountPath, parseServer.app);
 
-parseGraphQLServer.applyGraphQL(app); // Mounts the GraphQL API
-// parseGraphQLServer.applyPlayground(app); 
+parseGraphQLServer.applyGraphQL(app);
 
 const PORT = process.env.SERVER_PORT || 1337;
 app.listen(PORT, () => {
